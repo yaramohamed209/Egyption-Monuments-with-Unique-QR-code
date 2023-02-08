0d5c85f9a37d285a47bb937794498161035f5474
YARA <yara.yoka209@hotmail.com>
2023-02-08 08:28:50 +0200

    first

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import Blog from './screen/Blog'
+import Navigation from "./components/navigation";
+import Secondnav from "./components/secondnav";
+import Foot from "./components/foot";
+import Destination from "./screen/Destination";
+
+
+function App() {
+    return ( <div className = "App" >
+        <Navigation / >
+        <Secondnav / >
+        <Blog / >
+        <Destination / >
+        <Foot / >
+        <div > </div> </div>
+    );
+}
+
+
+export default App;
