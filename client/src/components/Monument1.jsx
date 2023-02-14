115b9c597d78e4d63c047927979b08f4e2543b64
YARA <yara.yoka209@hotmail.com>
2023-02-14 17:21:49 +0200

    update

---
+++ client/src/components/Monument1.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Monuments() {
+  return (
+    <div className="events">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+          
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="http://placehold.it/900x400"
+              alt=""
+            />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">Monuments</h1>
+            <p>
+              Some details about this monuments 
+              yaraaa
+            </p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Monuments;
