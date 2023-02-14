9f87e8794247db3f9339f7b27b5a00ed57436943
YARA <yara.yoka209@hotmail.com>
2023-02-14 17:25:13 +0200

    update

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -6,3 +6,4 @@ export { default as Monuments } from "./Monuments";
 export { default as Blog } from "./blog/Blog";
 export { default as Posts } from "./blog/Posts";
 export { default as Post } from "./blog/Post";
+export { default as Monument1 } from "./Monument1";
