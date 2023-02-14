ee645266a998ac44e375dce088358f4b4edde3a9
mayada ebrahim <mayadaebyahim50@gmail.com>
2023-02-14 04:37:52 +0200

    last

--- client/src/components/Monuments.jsx
+++ client/src/components/Monuments.jsx
@@ -6,6 +6,7 @@ function Monuments() {
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-7">
+          
             <img
               class="img-fluid rounded mb-4 mb-lg-0"
               src="http://placehold.it/900x400"
@@ -20,6 +21,7 @@ function Monuments() {
           </div>
         </div>
       </div>
+      
     </div>
   );
 }
