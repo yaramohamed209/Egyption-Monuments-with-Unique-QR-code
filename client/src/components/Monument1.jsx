ed1a0cbd1404271e5b1adc4feebff27e44748ea1
YARA <yara.yoka209@hotmail.com>
2023-02-17 00:55:05 +0200

    update

--- client/src/components/Monument1.jsx
+++ client/src/components/Monument1.jsx
@@ -13,26 +13,22 @@ function Monument1() {
             <div class="col-lg-7">
               <img
                 class="img-fluid rounded mb-4 mb-lg-0"
-                src={data.Monuments[0].image}
+                src="http://placehold.it/900x400"
                 alt=""
               />
             </div>
             <div class="col-lg-5">
               <h1 class="font-weight-light">Monumentss</h1>
-              <textarea
-                rows={5}
-                value={data.Monuments[1].description}
-                disabled
-              >
-              </textarea> 
-              <button onClick={() => speak({ text:value })}>Speak</button>
+              <div>
+                {data.Monuments[1].description}
+              </div> 
+              <button onClick={() => speak({ text:data.Monuments[1].description })}>Speak</button>
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
-
+}  
 
 export default Monument1;
--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import {
   Home,
   Monuments,
   Monument1,
+  Monument2,
   Events,
   Blog,
   Posts,
@@ -22,6 +23,7 @@ ReactDOM.render(
       <Route path="/" element={<Home />} />
       <Route path="/monuments" element={<Monuments />} />
       <Route path="/monument1" element={<Monument1 />} />
+      <Route path="/monument2" element={<Monument2 />} />
       <Route path="/events" element={<Events />} />
       <Route path="/blog" element={<Blog />}>
         <Route path="" element={<Posts />} />
