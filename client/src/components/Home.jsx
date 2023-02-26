5bad78013633372ce0fe8d89e89913ce2937afac
Mariam <mariam99moka99@gmai.com>
2023-02-26 02:27:10 +0200

    Homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,98 @@
 import React from "react";
-
-function Home() {
+function Home()
+ {
   return (
+
     <div className="home">
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-20">
-          <img src="./images/header-img.jpg" className="image-header" alt="" /> 
-    
+         
+    <h2 >WELCOME TO EGYPT</h2>
+        <div class="slider-frame">
+            <div class="slide-images">
+                    <div class="img-container">
+                        <img src="images/Highlights/national.jpg"alt=""/>
+                    </div>
+                    <div class="img-container">
+                        <img src="images/Highlights/cariomuseum.jpg"alt=""/>
+                    </div>
+                    <div class="img-container">
+                        <img src="images/Highlights/abdeenpalace.jpg"alt=""/>
+                    </div>   
+            </div>
+        </div>
+        
+
+        <div class="wrapper">
+    <div class="box box1">
+      <p>
+    Egypt is full of dreams, mysteries<br></br>
+    and memories.Go discover them !!
+    </p>
+    </div>
+    </div>
+    <div class="row ">
+     <div class="column">
+    <label class="textOverImage" >
+  <input type="checkbox"/>
+  <img src="images/Highlights/Nile.jpg" className= "egypt "alt=""/> 
+  <h2>Egyptian Nile</h2>
+  </label>
+    </div>
+<div class="column">
+<label class="textOverImage" >
+  <input type="checkbox"/>
+  <img src="images/Highlights/pyramids.jpg"  className= "egypt "alt=""/> 
+  <h2>Giza Pyramids</h2>
+</label>
+</div>
+<div class="column">
+<label class="textOverImage" >
+  <input type="checkbox"/>
+  <img src="images/Highlights/abusimbel.jpg"  className= "egypt"alt=""/> 
+  <h2>Abu Simbel Temples</h2>
+</label>
+</div>
+<div class="column">
+<label class="textOverImage" >
+  <input type="checkbox"/>
+  <img src="images/Highlights/islamic.jpg"  className= "egypt "alt=""/> 
+  <h2>Museum of Islamic Art</h2>
+</label>
+</div>
+</div>
+
+<div class="row ">
+<div class="column">
+<label class="textOverImage" >
+  <input type="checkbox"/>
+  <img src="images/Highlights/egyptianmuseum.jpg"  className= "egypt"alt=""/> 
+  <h2>Egyptian Museum</h2>
+</label>
+</div>
+<div class="column">
+<label class="textOverImage" >
+  <input type="checkbox"/>
+  <img src="images/Highlights/saintcatren.jpg"  className= "egypt"alt=""/> 
+  <h2>Saint Catherine Monastery </h2>
+</label>
+</div>
+<div class="column">
+<label class="textOverImage" >
+  <input type="checkbox"/>
+  <img src="images/Highlights/siwa.jpg"  className= "egypt"alt=""/> 
+  <h2>Siwa Oasis</h2>
+</label>
+</div>
+<div class="column">
+<label class="textOverImage" >
+  <input type="checkbox"/>
+  <img src="images/Highlights/redsea.jpg"  className= "egypt"alt=""/> 
+  <h2>The Red Sea</h2>
+</label>
+</div>
+</div>
     <div className= "images ">  
          <div  className= "image ">
             <img src="./images/t4.jpg"  className= "egypt-image "alt=""/> 
@@ -80,10 +165,10 @@ function Home() {
              </div>
          </div>          
      </div>
-          
           </div>
           
         </div>
+        
       </div>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import {
   Navigation,
   Footer,
@@ -13,6 +13,7 @@ import {
   Blog,
   Posts,
   Post,
+ 
 } from "./components";
 
 ReactDOM.render(
@@ -32,7 +33,7 @@ ReactDOM.render(
     <Footer />
   </Router>,
 
+
   document.getElementById("root")
 );
 
-serviceWorker.unregister();
