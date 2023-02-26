b52e92a615d288dd863b25c5fe82a956427683e3
YARA <yara.yoka209@hotmail.com>
2023-02-26 03:35:06 +0200

    m

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {
   Navigation,
@@ -9,10 +9,17 @@ import {
   Home,
   Monuments,
   Monument1,
+  Monument2,
+  Monument3,
+  Monument4,
+  Monument6,
+  Monument7,
+  Monument5,
   Events,
   Blog,
   Posts,
   Post,
+  Panorama
 } from "./components";
 
 ReactDOM.render(
