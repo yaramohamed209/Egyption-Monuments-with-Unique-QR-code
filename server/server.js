664fb5ba90feaa8ee553757c93594a32ab534a90
YARA <yara.yoka209@hotmail.com>
2023-03-08 22:03:33 +0200

    server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,33 @@
 import express from 'express';
-import data from './data.js';
+import  Path from 'path'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import monumentRouter from './routes/monumentRoutes.js';
+dotenv.config();
 
+  mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
-//
-app.get('/api/Monuments', (req, res) => {
-    res.send(data.Monuments);
-  });
-  
-  const port = process.env.PORT || 5000; 
-  app.listen(port, () => {
-    console.log(`server at http://localhost:${port}`);
+app.use('/api/seed', seedRouter);
+app.use('/api/monuments', monumentRouter);
 
-  });
+
+const __dirname = Path.resolve();
+app.use(express.static(Path.join(__dirname, '../client/build')));
+app.get('*', (req, res) =>
+  res.sendFile(Path.join(__dirname, '../client/build/index.html'))
+);
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server at http://localhost:${port}`);
+});
